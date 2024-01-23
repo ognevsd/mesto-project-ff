e8ecf71845b5faf41ee8d08d9cb6d8a36f3b522d
ognev <ognev.s.d@gmail.com>
2024-01-23 19:47:06 +0200

    Add animation to popups

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,11 @@
+// Adding animations to all popups when page is loaded
+const allPopups = document.querySelectorAll('.popup')
+allPopups.forEach(popup => {
+  popup.classList.add('popup_is-animated');
+})
+
 function openPopup(element) {
-  element.classList.add("popup_is-opened");
+  element.classList.add('popup_is-opened');
   document.addEventListener('keydown', closeWithEscBttn);
   const closeBttn = element.querySelector('.popup__close');
   closeBttn.addEventListener('click', () => {
