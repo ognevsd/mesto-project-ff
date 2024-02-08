9c53230dd2c11b4f925fdbb3f7fdd8a54d2a2945
ognev <ognev.s.d@gmail.com>
2024-02-08 19:31:52 +0200

    Move event handlers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import '../pages/index.css';
-import { requestCards, requestUserInfo } from './components/api.js';
-import { createNewCard, deleteCard } from './components/card.js';
-import { handleAddPlace, handleAvatarUpdate, handleFormEditProfile, handleLike } from './components/eventHandlers.js';
-import { closeWithButton, openPopup } from './components/modal.js';
+import { addLike, addNewCard, editProfileInfo, removeLike, requestCards, requestUserInfo, updateAvatar } from './components/api.js';
+import { createNewCard, deleteCard, toggleLike } from './components/card.js';
+import { closePopup, closeWithButton, openPopup } from './components/modal.js';
 import { clearValidation, enableValidation } from './components/validation.js';
 
 
@@ -33,9 +32,92 @@ const profileDescription = document.querySelector('.profile__description');
 const profileAvatar = document.querySelector('.profile__image');
 let userId = '';
 
-
 // Other elements
 const cardList = document.querySelector('.places__list');
+
+// Form inputs
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+const newAvatarInput = document.querySelector('#edit-avatar-url-input');
+const placeName = document.querySelector('#new-place-name-input');
+const placePicture = document.querySelector('#new-place-url-input');
+
+// Event handlers
+function handleFormEditProfile(evt) {
+  evt.preventDefault();
+
+  const nameInputValue = nameInput.value;
+  const jobInputValue = jobInput.value;
+
+  const submitButton = evt.submitter;
+  submitButton.textContent = 'Сохранение...';
+
+  editProfileInfo(nameInputValue, jobInputValue)
+    .then((res) => {
+      profileTitle.textContent = res.name;
+      profileDescription.textContent = res.about;
+      closePopup(popupEditProfile);
+    })
+    .catch(err => console.error(err))
+    .finally(() => {
+      submitButton.textContent = 'Сохранить'
+    })
+
+}
+
+function handleAddPlace(evt) {
+  evt.preventDefault();
+
+  const submitButton = evt.submitter;
+  submitButton.textContent = 'Сохранение...';
+
+  addNewCard(placeName.value, placePicture.value)
+    .then((res) => {
+      const newCard = createNewCard(res, deleteCard, handleLike, openImage, res.owner._id);
+      cardList.prepend(newCard);
+      closePopup(popupAddNewCard);
+      formNewCard.reset();
+    })
+    .catch(err => console.error(err))
+    .finally(() => {
+      submitButton.textContent = 'Сохранить'
+    })
+}
+
+function handleLike(likeElement, cardId) {
+  const hasLike = likeElement.querySelector('.card__like-button').classList.contains('card__like-button_is-active');
+  if (hasLike) {
+    removeLike(cardId)
+      .then((res) => {
+        toggleLike(likeElement, res.likes.length)
+      })
+      .catch(err => console.error(err))
+  } else {
+    addLike(cardId)
+      .then((res) => {
+        toggleLike(likeElement, res.likes.length)
+      })
+      .catch(err => console.error(err))
+  }
+}
+
+function handleAvatarUpdate(evt) {
+  evt.preventDefault()
+
+  const submitButton = evt.submitter;
+  submitButton.textContent = 'Сохранение...';
+
+  updateAvatar(newAvatarInput.value)
+    .then((res) => {
+      profileAvatar.style.backgroundImage = `url(${newAvatarInput.value})`;
+      closePopup(popupEditAvatar);
+      formEditProfileImage.reset()
+    })
+    .catch(err => console.error(err))
+    .finally(() => {
+      submitButton.textContent = 'Сохранить'
+    })
+}
 
 // Rendering initial data
 Promise.all([requestUserInfo(), requestCards()])
@@ -47,7 +129,6 @@ Promise.all([requestUserInfo(), requestCards()])
     addCards(cards, userId);
   })
   .catch(err => console.error(err))
-
 
 // Enabling validation
 const validationSettings = {
@@ -102,22 +183,7 @@ buttonOpenEditImagePopup.addEventListener('click', () => {
   openPopup(popupEditAvatar);
 })
 
-
 // Working with forms
 formEditProfile.addEventListener('submit', handleFormEditProfile);
 formNewCard.addEventListener('submit', handleAddPlace);
 formEditProfileImage.addEventListener('submit', handleAvatarUpdate);
-
-export {
-  cardList,
-  formEditProfile,
-  formEditProfileImage,
-  formNewCard,
-  openImage,
-  popupAddNewCard,
-  popupEditAvatar,
-  popupEditProfile,
-  profileAvatar,
-  validationSettings
-};
-
